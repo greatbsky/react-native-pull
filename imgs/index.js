7d5a14836f98dd960f81c9275802dc9ea05dc27e
architect.bian <greatarchitect@163.com>
2016-07-26 19:49:54 +0800

    submit all code, version1.0.4, npm version patch & npm publish,test ok

---
+++ imgs/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default {
+    // icon: require('./icon.png'),
+}
---
+++ style/index.css.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import {
+  StyleSheet
+} from 'react-native';
+
+export default StyleSheet.create({
+    wrap: {
+        flex: 1, backgroundColor: '#fefefe', flexDirection: 'column'
+    }
+});
